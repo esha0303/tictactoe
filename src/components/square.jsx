4c0e3fa3fc22e81ef2b5431a5f895abdcde64bb0
esha0303 <eshabudhamagar@gmail.com>
2024-10-22 01:09:34 +0530

    done

--- src/components/square.jsx
+++ src/components/square.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Squares({onClick}){
     return(
-        <>
+        <>git 
         <div className="p-10 border-2 border-black bg-gray-300" onClick={onClick}>
-            X
+            <div></div>
         </div>
         </>
     )
